f10d6394023deae5bebf8e5a0e969e447883e206
fubblea <ajay.arthur@gmail.com>
2024-05-05 19:28:25 -0700

    Added tmux conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,6 @@
 # Setup .config
 ln -s $(pwd)/config/* ${HOME}/.config/
 
+# Setup tmux
+ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
