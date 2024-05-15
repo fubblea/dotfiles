f4fa96c3baa2b992ee4acbe64bb3356dda05a3ee
fubblea <ajay.arthur@gmail.com>
2024-05-15 11:36:13 -0400

    Added .zshrc and updated boostrap.sh

---
+++ .zshrc
@@ -0,0 +1,39 @@
+source ~/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+
+# Other plugins
+antigen bundle tmux
+antigen bundle zoxide
+
+# Zoxide setup
+eval "$(zoxide init zsh)"
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+antigen theme robbyrussell
+
+# Tell Antigen that you're done.
+antigen apply
+
+. "$HOME/.cargo/env"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+alias lg="lazygit"
+alias pip="uv pip"
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,24 @@ ln -s $(pwd)/config/* ${HOME}/.config/
 # Setup tmux
 ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Setup zsh
+curl -L git.io/antigen > ${HOME}/antigen.zsh
+ln -s $(pwd)/.zshrc ${HOME}/.zshrc
+
+sudo apt update -y
+
+# Setup other utilities
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+sudo apt install zoxide
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
