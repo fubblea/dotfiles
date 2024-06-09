3e58cec7b0ae50ce2b79bb023f406cd8151b9ef1
fubblea <ajay.arthur@gmail.com>
2024-06-09 15:01:02 -0400

    Remove sudo from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Setup zsh
 ln -s $(pwd)/.zshrc ${HOME}/.zshrc
 
-sudo apt update -y
+apt update -y
 
 # Setup other utilities
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -19,10 +19,10 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+install lazygit /usr/local/bin
 
-sudo apt install zoxide
+apt install zoxide
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm -rf /opt/nvim
+tar -C /opt -xzf nvim-linux64.tar.gz
