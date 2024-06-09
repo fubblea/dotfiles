e6760406d8c63f4602936ea9501d852d2247e7d1
fubblea <ajay.arthur@gmail.com>
2024-06-09 15:42:48 -0400

    Install Rust

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ ln -s $(pwd)/config/* ${HOME}/.config/
 
 apt update -y
 
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # Setup tmux
 apt install tmux -y
 ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
@@ -16,16 +19,21 @@ chsh -s $(which zsh)
 ln -s $(pwd)/.zshrc ${HOME}/.zshrc
 
 # Setup other utilities
+
+# Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 install lazygit /usr/local/bin
 
+# Install zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
+# Install nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 rm -rf /opt/nvim
 tar -C /opt -xzf nvim-linux64.tar.gz
