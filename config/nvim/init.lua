5284e8102ef5d389060ec889e570a36cf03dfcb7
Ajay Anand <ajay.arthur@gmail.com>
2024-08-13 11:50:44 +0300

    Added inlay hints and changed theme to catpuccin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -78,6 +78,14 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+-- Inlay hints toggle
+vim.keymap.set(
+  "n",
+  "<leader>i",
+  "<cmd>lua vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())<CR>",
+  { desc = "View [I]nlay Hints" }
+)
+
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
@@ -708,13 +716,14 @@ require("lazy").setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    "folke/tokyonight.nvim",
+    "catppuccin/nvim",
+    name = "catpuccin",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme("tokyonight-night")
+      vim.cmd.colorscheme("catppuccin-mocha")
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi("Comment gui=none")
