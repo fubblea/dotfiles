9f30abdebe2e14d2d8996b3f461f1e92920db673
Ajay Anand <ajay.arthur@gmail.com>
2024-09-11 13:57:59 -0400

    Added nvim to paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,3 +39,6 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mkdir -p /opt/nvim
 mv nvim.appimage /opt/nvim/nvim
+echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.zshrc
+
