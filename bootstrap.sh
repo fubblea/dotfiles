47ea4186cd9d95666c56c892726ebb0ce18bce58
Ajay Anand <ajay.arthur@gmail.com>
2024-09-11 14:19:57 -0400

    Typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,8 +37,5 @@ curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh 
 apt install libfuse2
 wget https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
-sudo ln -s "${INSTALL_DIR}nvim-linux64/bin/nvim" /usr/local/bin/nvim
-mv nvim.appimage /opt/nvim/nvim
-echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.bashrc
-echo 'export PATH="$PATH:/opt/nvim/"' >> ~/.zshrc
+ln -s "${INSTALL_DIR}nvim-linux64/bin/nvim" /usr/local/bin/nvim
 
