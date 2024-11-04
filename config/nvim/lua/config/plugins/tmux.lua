44543197c0220f42318a72f19cf31b2a047377de
Ajay Anand <ajay.arthur@gmail.com>
2024-11-04 14:37:03 -0500

    Added a few plugins

---
+++ config/nvim/lua/config/plugins/tmux.lua
@@ -0,0 +1,15 @@
+return { -- tmux and nvim navigation
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    require("nvim-tmux-navigation").setup({
+      disable_when_zoomed = true, -- defaults to false
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        next = "<C-Space>",
+      },
+    })
+  end,
+}
