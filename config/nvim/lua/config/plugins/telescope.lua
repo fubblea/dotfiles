3c4523fa155331593343532b41b4ae389fa0f19b
Ajay Anand <ajay.arthur@gmail.com>
2024-12-17 14:20:39 -0500

    Added telescope workspace symbols keymap

--- config/nvim/lua/config/plugins/telescope.lua
+++ config/nvim/lua/config/plugins/telescope.lua
@@ -1,7 +1,9 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
     config = function()
         -- Keybindings
 
@@ -10,5 +12,6 @@ return {
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>ss', builtin.lsp_dynamic_workspace_symbols, { desc = 'Telescope workspace symbols' })
     end
 }
