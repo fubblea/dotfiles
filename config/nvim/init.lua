271da5dad279586f40c5f75ceca48934364b9cb9
Ajay Anand <ajay.arthur@gmail.com>
2025-09-14 16:39:53 -0400

    Kickstart 2025 (#4)
    * Setup mini
    
    * Removed unused file
    
    * Added LSP
    
    * Added other plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,20 @@
-if vim.g.neovide then
-  vim.g.neovide_scale_factor = 0.85
-end
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Import Options
+require("config.options")
+
+-- Import Keymaps
+require("config.keymaps")
+
+-- Import Autocommands
+require("config.autocommands")
+
+-- Import lazy
 require("config.lazy")
---
+++ config/nvim/lua/config/autocommands.lua
@@ -0,0 +1,43 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- Tabstop override
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "lua", "javascript", "typescript", "json", "yaml", "html", "css" },
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.softtabstop = 2
+	end,
+})
+
+-- Disable confirmation and run async formatting
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(event)
+		local map = function(keys, func, desc)
+			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
+
+		map("<leader>c", function()
+			vim.lsp.buf.format({
+				async = true,
+				filter = function(client)
+					-- If you only want to use a specific client for formatting:
+					-- return client.name == "null-ls"
+					return true
+				end,
+			})
+		end, "Format buffer")
+	end,
+})
